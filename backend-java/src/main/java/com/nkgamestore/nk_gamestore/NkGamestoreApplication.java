5694c33ee957f3bd3c7ef0f2b661a2f1ecd95d4f
JustAGameDev69 <vngkhang.icc@gmail.com>
2025-01-07 21:20:00 +0700

    Add backend

---
+++ backend-java/src/main/java/com/nkgamestore/nk_gamestore/NkGamestoreApplication.java
@@ -0,0 +1,13 @@
+package com.nkgamestore.nk_gamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkGamestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NkGamestoreApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/nkgamestore/nk_gamestore/NkGamestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkgamestore.nk_gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkGamestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
